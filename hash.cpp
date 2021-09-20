a4b83854e2acc9e6c83d3b0119ae489dbe5c56a5
Le Tang Phu Quy <42770385+letangphuquy@users.noreply.github.com>
2021-09-20 09:30:52 +0700

    Update hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -1,11 +1,11 @@
 const int MAX = 1e5 + 500;
 
 const int NUM_MODS = 4;
-const int MODS[4] = {MOD, 998244353, (int) 1e9 + 7, (int) 1e9 + 9};
+const int MODS[4] = {2004010501, 998244353, (int) 1e9 + 7, (int) 1e9 + 9};
 
 int pwr[NUM_MODS][MAX];
 void init() {
-	for (int t = 0; t < 4; t++) {
+	for (int t = 0; t < NUM_MODS; t++) {
 		pwr[t][0] = 1;
 		for (int i = 1; i < MAX; i++) pwr[t][i] = ((Int) pwr[t][i-1] * base) % MODS[t];
 	}
@@ -15,12 +15,12 @@ struct Value {
 	int d[NUM_MODS];
 	Value() {}
 	bool operator< (const Value& rhs) const {
-		for (int t = 0; t < 4; t++) 
+		for (int t = 0; t < NUM_MODS; t++) 
 			if (d[t] != rhs.d[t]) return d[t] < rhs.d[t];
 		return false;
 	}
 	bool operator== (const Value& rhs) const {
-		for (int t = 0; t < 4; t++) if (d[t] != rhs.d[t]) return false;
+		for (int t = 0; t < NUM_MODS; t++) if (d[t] != rhs.d[t]) return false;
 		return true;
 	}
 };
@@ -29,12 +29,12 @@ struct Hash {
 	int val[NUM_MODS][MAX];
 	Hash(string s) {
 		int n = s.size();
-		for (int t = 0; t < 4; t++)
+		for (int t = 0; t < NUM_MODS; t++)
 			for (int i = 1; i <= n; i++) val[t][i] = ((Int) val[t][i-1] * base + s[i-1]) % MODS[t];
 	}
 	Value get_hash(const int& l, const int& r) const {
 		Value res;
-		for (int t = 0; t < 4; t++) {
+		for (int t = 0; t < NUM_MODS; t++) {
 			res.d[t] = ((Int)-val[t][l-1] * pwr[t][r-l+1] + val[t][r]) % MODS[t];
 			if (res.d[t] < 0) res.d[t] += MODS[t];
 		}
