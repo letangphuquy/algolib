1a395716834ccb37734bb28d961cb2b068583f59
Le Tang Phu Quy <42770385+letangphuquy@users.noreply.github.com>
2021-11-02 11:47:49 +0700

    Create matrix_exponential.cpp

---
+++ Algebra/matrix_exponential.cpp
@@ -0,0 +1,27 @@
+struct Matrix {
+  int nrows, ncols;
+  int** val;
+  Matrix() {}
+  Matrix(int nrows, int ncols) : nrows(nrows), ncols(ncols) {
+    val = new int*[nrows];
+    for (int r = 0; r < nrows, r++) {
+      val[r] = new int[ncols];
+      memset(val[r], 0, sizeof(val[r]));
+    }
+  }
+  Matrix operator* (const Matrix& other) const {
+    assert(ncols == other.nrows);
+    Matrix res(nrows, other.ncols);
+    for (int i = 0; i < nrows; i++)
+      for (int j = 0; j < ncols; j++)
+        for (int k = 0; k < res.ncols; k++)
+          res.val[i][k] += val[i][j] * other.val[j][k];
+    return res;
+  }
+  Matrix operator^ (const int& expo) const {
+    if (expo == 1) return *this;
+    Matrix res = (*this) ^ (expo/2);
+    res = res * res;
+    if (expo % 2) res = res * (*this);
+    return res;
+};
