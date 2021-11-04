bcd122b6ce53a0a6dd91d8c98a7233eb9941024c
Le Tang Phu Quy <42770385+letangphuquy@users.noreply.github.com>
2021-11-04 10:52:26 +0700

    Update 2-SAT.cpp

--- Graph/2-SAT.cpp
+++ Graph/2-SAT.cpp
@@ -6,16 +6,23 @@ typedef long long Int;
 const int MAX = 2e5 + 500;
 
 int num_nodes, num_conditions, pr1[MAX], pr2[MAX];
+int num_vars;
 
-int NOT(const int& t) { if (t > num_nodes) return t-num_nodes; return t+num_nodes; }
 vector<int> adj[MAX];
-void equal(const int& x, const int& y) {
+int NOT(const int& t) { return t^1; }
+void VRAI(const int& u) { adj[NOT(u)].push_back(u); }
+void FAUX(const int& u) { adj[u].push_back(NOT(u)); }
+void OR(const int& x, const int& y) {
+	adj[NOT(x)].push_back(y);
+	adj[NOT(y)].push_back(x);
+}
+void EQUAL(const int& x, const int& y) {
 	adj[x].push_back(y);
 	adj[y].push_back(x);
 	adj[NOT(x)].push_back(NOT(y));
 	adj[NOT(y)].push_back(NOT(x));
 }
-void nequal(const int& x, const int& y) { equal(x, NOT(y)); }
+void NEQUAL(const int& x, const int& y) { EQUAL(x, NOT(y)); }
 
 int ord[MAX], low[MAX], vis_turn = 0;
 int num_comps = 0, comp[MAX]; 
@@ -41,15 +48,16 @@ void tarjan(int u) {
 bool ans[MAX];
 bool solve()
 {
-    for (int u = 1; u <= 2*num_nodes; u++) 
+    num_vars = 2 * num_nodes;
+    for (int u = 1; u <= num_vars; u++) 
         adj[u].clear(), 
         comp[u] = ord[u] = low[u] = 0;
     for (int i = 1; i <= num_conditions; i++) 
-        nequal(pr1[i], pr2[i]);
+ 	OR(pr1[i], pr2[i]);
     ::num_comps = ::vis_turn = 0;
-    for (int u = 1; u <= 2*num_nodes; u++) 
+    for (int u = 0; u < num_vars; u++) 
         if (!ord[u]) tarjan(u);
-    for (int u = 1; u <= num_nodes; u++) {
+    for (int u = 0; u < num_vars; u++) {
 	ans[u] = comp[NOT(u)] > comp[u];
         if (comp[u] == comp[NOT(u)]) return false;
     }
