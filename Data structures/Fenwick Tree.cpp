98e851311b204e5b5121a6731700c4099b4a84d4
Le Tang Phu Quy <42770385+letangphuquy@users.noreply.github.com>
2021-12-09 15:02:55 +0700

    Update Fenwick Tree.cpp

--- Data structures/Fenwick Tree.cpp
+++ Data structures/Fenwick Tree.cpp
@@ -2,13 +2,13 @@ struct Data {
     Data() {}
     Data operator+ (const Data& rhs) const { return *this; }
 };
-struct FenwickTree {
-    int range; 
-    Data* tree;
+template<typename T> struct FenwickTree {
+    int range;
+    T* tree;
     FenwickTree() {}
-    void resize() { tree = new Data[range+1]; fill(tree, tree+1+range, Data()); } 
+    void resize() { tree = new T[range+1]; fill(tree, tree+1+range, T()); }
     FenwickTree(int range) : range(range) { resize(); }
     void init(int n) { range = n; resize(); }
-    void update(int x, const Data& add) { for (;x <= range; x += x&-x) tree[x] = tree[x] + add; }
-    Data query(int x) { Data res; for (;x; x &= x-1) res = res + tree[x]; return res; }
+    void update(int x, const T& add) { for (;x <= range; x += x&-x) tree[x] = tree[x] + add; }
+    T query(int x) { T rex; for (;x; x &= x-1) rex = rex + tree[x]; return rex; }
 };
