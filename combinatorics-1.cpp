34a2e8aab9d10b576404aead634718668a05744b
Le Tang Phu Quy <42770385+letangphuquy@users.noreply.github.com>
2022-01-15 10:18:44 +0700

    Update combinatorics-1.cpp

--- combinatorics-1.cpp
+++ combinatorics-1.cpp
@@ -1,21 +1,21 @@
 const int MOD = 1e9 + 7;
 typedef long long Int;
+void add(int &a, const int& b) { if ((a+=b) >= MOD) a -= MOD; }
+int product(const int& a, const int& b) { return (Int)a*b % MOD; }
 
 int pwr_mod(int a, int n) {
 	if (!n) return 1;
-	int res = pwr_mod(a,n/2);
-	res = ((Int) res*res) % MOD;
-	if (n&1) res = ((Int) res*a) % MOD;
-	return res;
+	return product(pwr_mod(product(a,a), n/2), (n%2) ? a : 1);
 }
+
 const int N = 1e6 + 100;
 int fac[N], inv[N];
 void prepare(int n) {
 	fac[0] = 1;
-	for (int i = 1; i <= n; i++) fac[i] = ((Int) i * fac[i-1]) % MOD;
+	for (int i = 1; i <= n; i++) fac[i] = product(fac[i-1], i);
 	inv[n] = pwr_mod(fac[n], MOD-2);
-	for (int i = n-1; i >= 0; i--) inv[i] = ((Int) (i+1) * inv[i+1]) % MOD;
+	for (int i = n-1; i >= 0; i--) inv[i] = product(inv[i+1], i);
 }
 int combi(int n, int k) {
-	return ((Int) fac[n] * (((Int) inv[k] * inv[n-k]) % MOD)) % MOD;
+	return product(fac[n], product(inv[k], inv[n-k]));
 }
