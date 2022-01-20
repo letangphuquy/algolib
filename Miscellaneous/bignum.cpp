cc95e042ca3813dd100b0530164ff19ea731e5d7
Le Tang Phu Quy <42770385+letangphuquy@users.noreply.github.com>
2022-01-20 16:12:57 +0700

    Update bignum.cpp

--- Miscellaneous/bignum.cpp
+++ Miscellaneous/bignum.cpp
@@ -1,15 +1,9 @@
+/* libraries
 #include<bits/stdc++.h>
 using namespace std;
-
-//typedef unsigned long long Int;
 typedef long long Int;
-typedef long double Real;
-
-const int MOD = 2004010501; //MOD > 2e9
-
-//Bignum tu viet, pro no phai the
+*/
 constexpr int base = 1e9;
-//void mod_add(int& var, const int& val) { var += val; if (var >= base) var -= base; }
 struct Num {
 	vector<int> digits;
 	Num() { digits.assign(1,0); }
@@ -19,12 +13,12 @@ struct Num {
 	}
 	void print() const {
 		for (auto it = digits.rbegin(); it != digits.rend(); it++) {
-			if (it != digits.rbegin()) {
-				int d = 0; for (int tmp = *it; tmp; tmp /= 10) d++;
-				if (!d) d ^= 1;
-				for(d = 9-d; d --> 0; ) cout << 0;
-			}
-			cout << (*it);
+			if (it != digits.rbegin())
+// 				printf("%09d", *it),
+				cout << setw(9) << setfill(0) << (*it);
+			else 
+// 				printf("%d", *it),
+				cout << (*it);
 		}
 	}
 };
@@ -42,7 +36,7 @@ bool operator<= (const Num& a, const Num& b) {
 	return true;
 }
 int get_digit(const vector<int>& x, int pos) {
-	return (pos < x.size()) ? x[pos] : 0;
+	return (pos < (int) x.size()) ? x[pos] : 0;
 }
 
 Num operator+ (const Num& a, const Num& b) {
@@ -110,77 +104,4 @@ pair<Num,Num> operator/ (Num a, const Num& b) {
 Int lcm(Int a, Int b) { return ((a /= __gcd(a,b)) *= b); }
 Num lcm2(Int a, Int b) {
 	return Num(a /= __gcd(a,b)) * Num(b);
-}
-
-int main()
-{
-	#define task "coins"
-	if (fopen(task".inp", "r")) {
-		freopen(task".inp", "r", stdin);
-		freopen(task".out", "w", stdout);
-	}
-
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-	if (false)
-	{
-//		(Num(1e9) - Num()).print();
-//		Num a(1e9), b(1e9+1);
-//		Num c = a * b;
-//		cerr << "Test : "; c.debug(); cerr << '\n';
-//		c = c + (a-1);
-//		cerr << "Test : "; c.debug(); cerr << '\n';
-//		auto divi = c / a;
-//		cerr << "Quotient, Rem = "; divi.first.debug(); cerr << " | "; divi.second.debug(); cerr << '\n';
-	}
-	
-	int num_coins, num_types;
-//	scanf("%d %d", &num_coins, &num_types);
-	cin >> num_coins >> num_types;
-	Int thickness[num_coins];
-//	for (int i = 0; i < num_coins; i++) scanf("%lld", thickness+i);
-	for (int i = 0; i < num_coins; i++) cin >> thickness[i];
-	
-	vector<Num> big_bases;
-	vector<Int> small_bases;
-	for (int a = 0; a < num_coins; a++)
-		for (int b = a+1; b < num_coins; b++)
-			for (int c = b+1; c < num_coins; c++)
-				for (int d = c+1; d < num_coins; d++) {
-					Num basis = lcm2(lcm(thickness[a],thickness[b]), lcm(thickness[c],thickness[d]));
-					if (basis.digits.size() <= 2) 
-						small_bases.push_back(lcm(lcm(thickness[a],thickness[b]), lcm(thickness[c],thickness[d])));
-					else big_bases.push_back(basis);
-				}
-//	cout << bases.size() << " wow so many\n";
-//	for (auto basis : bases) basis.print(), cout << '\n';
-				
-	for (Int height; num_types --> 0;) {
-//		scanf("%lld", &height);
-		cin >> height;
-		Num hei(height);
-		Num lower, higher;
-		higher.digits.assign(4, base-1);
-		for (auto basis : big_bases) {
-			if (hei < basis) {
-				higher = min(higher,basis);
-				continue;
-			}
-			pair<Num,Num> division = hei / basis;
-			Num _lower = hei - division.second;
-			lower = max(lower, _lower);
-			Num _higher = _lower;
-			if (division.second.digits.size() > 1 or division.second.digits.front() > 0)
-				_higher = _higher + basis;
-			higher = min(higher, _higher);
-		}
-		for (auto basis : small_bases) {
-			Int _lower = (height / basis) * basis;
-			lower = max(lower, (Num) _lower);
-			Int _higher = _lower + (_lower < height ? basis : 0);
-			higher = min(higher, (Num) _higher);
-		}
-		lower.print(); cout << ' '; higher.print(); cout << '\n';
-//		cerr << "Finished a testcase\n";
-	}
 }
