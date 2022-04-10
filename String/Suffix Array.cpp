d5f7bb9d821567c8b22337c53101a751423365dc
Le Tang Phu Quy <42770385+letangphuquy@users.noreply.github.com>
2022-04-10 17:12:14 +0700

    Update Suffix Array.cpp
    get rid of vectors

--- String/Suffix Array.cpp
+++ String/Suffix Array.cpp
@@ -1,3 +1,4 @@
+
 string S;
 const int T = 1e6+N;
 const int LG = 21;
@@ -11,43 +12,59 @@ void build_suffix_array() {
 	const int n = S.size();
 	S = "." + S; //1-based index
 	/* prefix_doubling */
-	vector<int> by_second[T];
-	int ids[T], cnt_le[T], ptr[T];
-//	for (int i = 1; i <= n; i++) ids[i] = i;
+	int ids[T], cnt_le[T], ptr[T], ids2[T];
+	for (int i = 1; i <= n; i++) ids[i] = i;
 	for (int t = 0; t <= LG; t++) {
-		if (t == 0)
+		if (t == 0) {
 			for (int i = 1; i <= n; i++) val[i] = make_pair(S[i],0);
+			sort(ids+1, ids+1+n, cmp);
+		}
 		else {
-			for (int i = 1, j = (1<<(t-1))+1; i <= n; i++, j++) {
+			for (int i = 1, j = (1<<(t-1))+1; i <= n; i++, j++)
 				val[i] = make_pair(rnk[i], (j <= n) ? rnk[j] : 0);
+			//sort by second
+			int mx = 0;
+			for (int i = 1; i <= n; i++) 
+				maximize(mx, val[i].second),
+				++cnt_le[val[i].second];
+			for (int i = 0; i <= mx; i++) {
+				ptr[i] = 1;
+				if (i > 0) 
+					ptr[i] += cnt_le[i-1],
+					cnt_le[i] += cnt_le[i-1];			
 			}
+			for (int i = 1; i <= n; i++) {
+				ids2[ptr[val[ids[i]].second]++] = ids[i];
+			}
+			for (int i = 0; i <= mx; i++) cnt_le[i] = 0;
+			for (int i = 1; i <= n; i++) ids[i] = ids2[i];
+			//sort by first
+			mx = 0;
+			for (int i = 1; i <= n; i++) 
+				maximize(mx, val[i].first),
+				++cnt_le[val[i].first];
+			for (int i = 0; i <= mx; i++) {
+				ptr[i] = 1;
+				if (i > 0) 
+					ptr[i] += cnt_le[i-1],
+					cnt_le[i] += cnt_le[i-1];			
+			}
+			for (int i = 1; i <= n; i++) {
+				ids2[ptr[val[ids[i]].first]++] = ids[i];
+			}
+			for (int i = 0; i <= mx; i++) cnt_le[i] = 0;
+			for (int i = 1; i <= n; i++) ids[i] = ids2[i];
 		}
-		int mx = 0;
-		for (int i = 1; i <= n; i++) 
-			maximize(mx, val[i].first),
-			++cnt_le[val[i].first],
-			by_second[val[i].second].push_back(i);
-		for (int i = 1; i <= mx; i++) {
-			ptr[i] = 1 + cnt_le[i-1];
-			cnt_le[i] += cnt_le[i-1];			
-		}
-		for (int i = 0; i <= n; i++) {
-			for (auto j : by_second[i])
-				ids[ptr[val[j].first]++] = j;
-		}
-		for (int i = 1; i <= n; i++) 
-			by_second[val[i].second].clear();
-		for (int i = 1; i <= mx; i++) cnt_le[i] = 0;
 		for (int i = 1; i <= n; i++) {
 			sa[i] = ids[i];
 			rnk[ids[i]] = i;
 			if (i > 1)
 				rnk[ids[i]] = rnk[ids[i-1]] + cmp(ids[i-1], ids[i]);
 		}
-		if (rnk[ids[n]] == n) break;
 //		cerr << "ITER " << t << '\n';
 //		for (int i = 1; i <= n; i++)
 //			cerr << sa[i] << ": " << S.substr(sa[i], n-sa[i]+1) << '\n';
+		if (rnk[ids[n]] == n) break;
 	}
 	for (int i = 1; i <= n; i++) rnk[sa[i]] = i;
 	/* kasai */
