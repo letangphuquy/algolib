60463b39362c0d26771c7404b9fee51b26b37ce5
Le Tang Phu Quy <42770385+letangphuquy@users.noreply.github.com>
2022-06-24 09:38:38 +0700

    Create latex_to_markdown.cpp

---
+++ GenTest/PolygonHelpers/latex_to_markdown.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	freopen("problem.tex", "r", stdin);
+	freopen("problem.md", "w", stdout);
+	string line;
+	while (getline(cin,line)) {
+		for (auto& c : line) if (c == '$') c = '~';
+		cout << line << '\n';
+	}
+}
