3ba33303a3374acd7f1744aec3344763e475ef79
Le Tang Phu Quy <42770385+letangphuquy@users.noreply.github.com>
2022-06-24 16:06:02 +0700

    Update latex_to_markdown.cpp
    still editing
    
    https://lqdoj.edu.vn/problem/markdown

--- GenTest/PolygonHelpers/latex_to_markdown.cpp
+++ GenTest/PolygonHelpers/latex_to_markdown.cpp
@@ -5,8 +5,10 @@ int main() {
 	freopen("problem.tex", "r", stdin);
 	freopen("problem.md", "w", stdout);
 	string line;
+	string text;
 	while (getline(cin,line)) {
-		for (auto& c : line) if (c == '$') c = '~';
-		cout << line << '\n';
+// 		for (auto& c : line) if (c == '$') c = '~';
+// 		cout << line << '\n';
+		text += line;
 	}
 }
