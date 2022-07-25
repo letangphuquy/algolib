a70bac8c1a213e16176342a6bac2a40eed80f043
Le Tang Phu Quy <42770385+letangphuquy@users.noreply.github.com>
2022-07-25 19:36:47 +0700

    Update SegmentTree (traditional).cpp
    - Update to match current style
    - Add comments for newbie

--- Data structures/SegmentTree (traditional).cpp
+++ Data structures/SegmentTree (traditional).cpp
@@ -4,60 +4,63 @@ Perform 10^6 queries of two types :
 1) increase a[l..r] by c : a[l] += c, a[l+1] += c, ..., a[r] += c
 2) compute F(l,r) = sigma(a[i] * b[i]) = a[l] * b[l] + a[l+1] * b[l+1] + ... + a[r] * b[r]
 */
+const int MAXN = 1e5 + 50;
 struct Node {
-	int sum_values, sum_coefs, lazy;
+	int sum_values;
 	Node() {};
 };
 void push_up(Node& x, const Node& L, const Node& R) {
 	x.sum_values = L.sum_values + R.sum_values;
 }
 struct SegmentTree {
-	#define mid (l+r>>1)
 	int range;
+	int lazy[4 * MAXN]; // thông tin lazy
+	int sum_coefs[4 * MAXN]; //thông tin cố định - lưu ở đây
 	Node nodes[4 * MAXN];
 	void build(int x, int l, int r, const int coefs[]) {
 		if (l == r)
-			nodes[x].sum_coefs = coefs[r];
+			sum_coefs[x] = coefs[r];
 		else {
-			build(x*2, l, mid, coefs);
-			build(x*2+1, mid + 1, r, coefs);
-			nodes[x].sum_coefs = nodes[x*2].sum_coefs + nodes[x*2+1].sum_coefs;
+			int m = (l+r)/2;
+			build(2*x, l,m, coefs);
+			build(2*x+1, m+1,r, coefs);
+			sum_coefs[x] = sum_coefs[2*x] + sum_coefs[2*x+1];
 		}
-		cerr << "[" << l << ',' << r << "] : " << nodes[x].sum_coefs << '\n';
+		cerr << "[" << l << ',' << r << "] : " << sum_coefs[x] << '\n'; //debug - xóa dòng này
 	}
 	SegmentTree(int range, const int a[]) : range(range) {
 		build(1, 1, range, a);
 	}
 	void push_down(int x) {
-		nodes[x*2].sum_values += nodes[x].lazy * nodes[x*2].sum_coefs;
-		nodes[x*2+1].sum_values += nodes[x].lazy * nodes[x*2+1].sum_coefs;
-		nodes[x*2].lazy += nodes[x].lazy;
-		nodes[x*2+1].lazy += nodes[x].lazy;
-		nodes[x].lazy = 0;
+		if (!lazy[x]) return ;
+		for (int y = 2*x; y <= 2*x+1; y++) {
+			nodes[y].sum_values += lazy[x] * sum_coefs[y];
+			lazy[y] += lazy[x];
+		}
+		lazy[x] = 0;
 	}
 	int query(int x, int l, int r, const int& L, const int& R) {
-		if (l > R or L > r) return 0;
+		if (l > r or l > R or L > r or L > R) return 0;
 		if (L <= l and r <= R) return nodes[x].sum_values;
-		push_down(x);
-		int result = query(x*2, l, mid, L, R) + query(x*2+1, mid+1, r, L, R);
+		int m = (l+r)/2; push_down(x); 
+		int result = query(x*2, l,m, L, R) + query(x*2+1, m+1,r, L, R);
 		/*
 		Node result;
 		push_up(result, query(x*2,l,mid,L,R), query(x*2+1,mid+1,r,L,R));
 		*/
 		return result;
-		
 	}
 	void range_update(int x, int l, int r, const int& L, const int& R, const int& delta) {
-		if (l > R or L > r) return ;
+		if (l > r or l > R or L > r or L > R) return ;
 		if (L <= l and r <= R) {
-			nodes[x].sum_values += delta * nodes[x].sum_coefs;
-			nodes[x].lazy += delta;
+			nodes[x].sum_values += delta * sum_coefs[x];
+			lazy[x] += delta;
 			return;
 		}
-		push_down(x);
-		range_update(x*2, l, mid, L, R, delta);
-		range_update(x*2+1, mid+1, r, L, R, delta);
-		nodes[x].sum_values = nodes[x*2].sum_values + nodes[x*2+1].sum_values;
+		int m = (l+r) / 2; push_down(x);
+		range_update(2*x, l,m, L, R, delta);
+		range_update(2*x+1, m+1,r, L, R, delta);
+		nodes[x].sum_values = nodes[2*x].sum_values + nodes[2*x+1].sum_values;
 	}
 	int query(const int& l, const int& r) {
 		return query(1, 1, range, l, r);
@@ -65,5 +68,4 @@ struct SegmentTree {
 	void range_update(const int& l, const int& r, const int& delta) {
 		range_update(1, 1, range, l, r, delta);
 	}
-	#undef mid
 };
